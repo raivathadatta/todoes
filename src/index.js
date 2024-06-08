b893351ba7a30c8bb98c0b03d478fd174b9fb2d6
raivatha datta <raivatha.28.2@mountblue.tech>
2024-06-08 23:07:34 +0530

    clean ed code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", function () {
 
     let todoes = []
@@ -18,11 +17,34 @@ document.addEventListener("DOMContentLoaded", function () {
     let completed_list = document.getElementById("completed")
     let clear_component = document.getElementById("clear_component")
 
+
+    title.innerHTML = "todos"
+    label_inuput.innerHTML = "&#8964;"
+    count_element.innerHTML = "0 items left!"
+    title.style = "color: #B83F45;text-align:center"
+    label_inuput.className = "text-gray-500 outline-none text-2xl"
+    input_element.className = ".text-gray-500   text-[20px]  mr-[5px] w-[80%] p-4"
+    input_element.style.outline = "none"
+
+
+
     all_list.addEventListener("click", allCheckedList);
     active_list.addEventListener("click", activeCheckedList)
     clear_component.addEventListener("click", clearCompletedList)
     completed_list.addEventListener("click", completedCheckedList);
+    label_inuput.addEventListener("click", () => toggleCheckedItem)
+    input_element.addEventListener("click", () => {
+        console.log("click")
+        input_element_parent.style = "border: 2px solid #D08383"
+        input_element.addEventListener("blur", () => {
+            input_element_parent.style = "border: none"
+        })})
 
+    input_element.addEventListener("keypress", (event) => {
+        if (event.key === "Enter") {
+            addTotoList(input_element.value)
+        }
+    });
 
     function clearCompletedList() {
         for (let index = ul.children.length - 1; index >= 0; index--) {
@@ -31,11 +53,8 @@ document.addEventListener("DOMContentLoaded", function () {
             if (input.checked) {
                 li.remove()
             }
-
         }
     }
-
-
 
     function completedCheckedList() {
         console.log("hello")
@@ -47,122 +66,57 @@ document.addEventListener("DOMContentLoaded", function () {
             } else {
                 li.style.display = "block";
             }
-
         }
     }
 
-
-
-
-
-
-    title.innerHTML = "todos"
-    label_inuput.innerHTML = "&#8964;"
-    count_element.innerHTML = "0 items left!"
-    title.style = "color: #B83F45;text-align:center"
-    label_inuput.style = "font-size:40px; color:gray; outline: none;"
-
-    input_element.style = "margin-right:5px;font-size:20px; color:gray; outline: none;"
-
-
-    label_inuput.addEventListener("click", () => {
+    function toggleCheckedItem() {
         console.log(`checked ${isChecked}`)
         isChecked = !isChecked
         resetCounter()
-
         for (let index = 0; index < ul.children.length; index++) {
-
-
             console.log(ul.children[index])
             let li_input = ul.children[index].querySelector("input");
             let li_label = ul.children[index].querySelector("label");
-            console.log(li_input)
-
             li_input.checked = isChecked
             li_label.style = isChecked ? "text-decoration: line-through; color:gray; font-size:20px" : "text-decoration: none;  font-size:20px"
-
-
         }
-    })
-
-    input_element.addEventListener("click", () => {
-        console.log("hello")
-        input_element_parent.style = "border: 2px solid #D08383"
-
-        input_element.addEventListener("blur", () => {
-            // input_element_parent.className = "border-none"
-            input_element_parent.style = "border: none"
-
-
-        })
-
-    })
-
-
-    input_element.addEventListener("keypress", (event) => {
-        if (event.key === "Enter") {
-
-            addTotoList(input_element.value)
-        }
-    });
-
-
+    }
 
     function addTotoList(event) {
 
         let input = event
+
         if (input.length > 1) {
             todoes.push(input)
             let li = document.createElement("li")
             let label = document.createElement('label');
             var inputElemnt = document.createElement("input");
-
-            inputElemnt.style = "margin-right: 10px; padding-top: 10px;"
-            label.style = "margin-bottom:10px; font-size:20px  "
-            li.style = "border-bottom: 1px  solid gray;margin-top :8px; padding-bottom: 10px;"
-            // li.className= "bb-5 color-gray mt-2 pb-4 "
-
+            inputElemnt.className = "mr-3 pt-2"
+            label.className = "mb-3 text-2xl"
+            li.className = "border-b-[1px] border-gray mt-[8px] pb-[10px] "
             inputElemnt.setAttribute("type", "checkbox");
             label.textContent = input
             li.appendChild(inputElemnt)
             li.appendChild(label)
             let button = document.createElement("button");
 
-            // button.style.display = "none"
 
             button.textContent = "x"
-            button.style = "float: right; margin-right:5px; color:gray; font-size:30px; display:none"
+            button.className = "float-right mr-[5px] text-[gray]  text-[30px] hidden"
 
 
-
-            li.addEventListener("dblclick", (event) => {
-                console.log(event, event)
-                li.removeEventListener("mouseenter", (_) => {
-                    console.log('mouse enter')
-                })
-                editContent(event)
-
-
-            })
             button.addEventListener("click", (event) => {
                 if (!inputElemnt.checked) {
                     console.log('its uncheck')
                     countDecrementer()
                 }
-
                 event.target.parentNode.remove();
-                // console.log(ul.childNodes.length, 'childNodes', ul.children.length, "children length")
             })
-
-
-
             inputElemnt.addEventListener("click", () => {
                 if (inputElemnt.checked) {
                     console.log('its check')
                     label.style = "text-decoration: line-through; color:gray; font-size:20px"
-
                     countDecrementer()
-
                 } else {
                     console.log('its uncheck')
                     label.style = "margin-bottom:10px; font-size:20px; text-decoration: none; color:black;"
@@ -171,23 +125,20 @@ document.addEventListener("DOMContentLoaded", function () {
             })
 
             li.appendChild(button)
-            // div.append(li)
             ul.appendChild(li)
 
             li.addEventListener("mouseenter", (event) => {
-
-                button.style = "float: right; margin-right:5px; color:gray; font-size:30px; display:block"
-
-
+                button.classList.remove("hidden")
             })
             li.addEventListener("mouseleave", (event) => {
-
-                // button.classList.add('hidden')
-                button.style = "float: right; margin-right:5px; color:gray; font-size:30px; display:none"
-
-
+                button.classList.add("hidden")
             })
+            li.addEventListener("dblclick", (event) => {
+                editContent(event)
+            })
+
             input_element.value = ""
+
             countIncrementer()
         }
 
@@ -196,15 +147,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
     function resetCounter() {
-
         console.log(ul.children.length, "ul.children.length")
         activeCount = isChecked ? 0 : ul.children.length
         console.log(activeCount)
         count_element.innerHTML = activeCount + " items left"
-
     }
-
-
 
     function countIncrementer() {
         activeCount++;
@@ -232,9 +179,7 @@ document.addEventListener("DOMContentLoaded", function () {
         let label = li.querySelector('label')
         let checkbox = li.querySelector('input')
         let button = li.querySelector('button')
-        checkbox.style = "display:none"
-        button.classList = "display:none"
-
+        checkbox.classList.add("hidden")
         let currentText = label.textContent
 
         let input = document.createElement('input')
@@ -242,63 +187,37 @@ document.addEventListener("DOMContentLoaded", function () {
         input.type = "text"
         input.value = currentText
         input.style = "margin-left: 15px;outline: none; width: 100%; "
-        label.replaceWith(input)
-        // input.className = "w-full border-none"
-        input.focus()
 
+        label.replaceWith(input)
+
+        input.focus()
         input.addEventListener('keypress', (e) => {
             if (e.key === 'Enter') {
                 label.textContent = input.value
-                label.style = "margin-bottom:10px; font-size:20px;display:''; margin-left:10px; margin-right:10px"
-
-
+                checkbox.classList.remove("hidden")
+                button.classList.remove("hidden")
                 input.parentNode.replaceChild(label, input)
-
-                checkbox.style = "display:''"
-                button.classList = "display:''"
-                // label.style = "margin-bottom:10px; font-size:20px;display:''; margin-left:10px; margin-right:10px"
-
-
             }
         })
         input.addEventListener("blur", () => {
             label.textContent = label.textContent
             input.parentNode.replaceChild(label, input)
-
-            checkbox.style = "display:''"
-            // label.style = "margin-bottom:10px; font-size:20px;display:''; margin-left:10px; margin-right:10px"
-            button.classList = "display:''"
-
-
-
+            checkbox.classList.remove("hidden")
+            button.classList.remove("hidden")
         })
     }
-
-
-
 
     function allCheckedList() {
         console.log("hello")
         for (let index = 0; index < ul.children.length; index++) {
             let li = ul.children[index]
             li.style = "border-bottom: 1px  solid gray;margin-top :8px; padding-bottom: 10px; display: ''"
-
-
         }
     }
-
-
-
-
     function activeCheckedList() {
-
         for (let index = 0; index < ul.children.length; index++) {
-            // li.style.display = ''
-
             let li = ul.children[index]
-
             let input = ul.children[index].querySelector("input")
-
             if (input.checked) {
                 console.log("checked")
                 li.style.display = "none"
